2f9c390ca7505e10e2034f0ec359d169ce4114a2
adam-kuhn <adam.kuhn20@gmail.com>
2020-03-28 14:19:38 +0200

    move a number of boxes around the grid, create initial flyer class

--- organicFlocking/index.js
+++ organicFlocking/index.js
@@ -1,27 +1,67 @@
 const canvas = document.getElementById('flocking-behaviour')
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = '#FF2304'
-ctx.fillRect(0, 0, 150, 75)
-const intervalId = setInterval(() => moveRectangel(), 0)
-let x = 0
-let y = 0
-function moveRectangel () {
-  ctx.clearRect(x, y, 150, 75)
-  if (x < 150 && y === 0) {
-    x += 10
+class FlyingAgents {
+  constructor (id, startingPosition, fillStyle) {
+    this.id = id
+    this.dimensions = {
+      width: 10,
+      height: 10
+    }
+    this.currentPosition = {
+      xPosition: startingPosition,
+      yPosition: startingPosition
+    }
+    this.fillStyle = fillStyle
   }
-  if (x === 150 && y < 75) {
-    y += 5
+  setCurrentPosition (x, y) {
+    this.currentPosition.xPosition = x
+    this.currentPosition.yPosition = y
   }
-  if (y === 75 && x <= 150) {
-    x -= 10
+  getOrientation () {
+    // return direction
   }
-  if (x === 0 && y <= 75) {
-    y -= 5
+  getPosition () {
+    // return current position
   }
-  if (x === 0 && y === 0) {
-    clearInterval(intervalId)
-  }
-  ctx.fillRect(x, y, 150, 75)
+}
+
+const numberOfFliers = 10
+let flyingAgents = []
+for (let i = 0; i < numberOfFliers; i++) {
+  const startingPosition = i * 10
+  const fillStyle = `#FF${i * 1000}`
+  const newFlyer = new FlyingAgents(i, startingPosition, fillStyle)
+  flyingAgents = [...flyingAgents, newFlyer]
+}
+
+// ctx.fillStyle = '#FF2304'
+
+flyingAgents.forEach((flyer) => {
+  ctx.fillStyle = flyer.fillStyle
+  ctx.fillRect(flyer.currentPosition.xPosition, flyer.currentPosition.yPosition, flyer.dimensions.width, flyer.dimensions.height)
+})
+
+setInterval(() => moveRectangel(flyingAgents), 100)
+function moveRectangel (flyingAgents) {
+  flyingAgents.forEach((flyer, idx) => {
+    ctx.clearRect(flyer.currentPosition.xPosition, flyer.currentPosition.yPosition, flyer.dimensions.width, flyer.dimensions.height)
+    let {xPosition} = flyer.currentPosition
+    let {yPosition} = flyer.currentPosition
+    if (xPosition < 150 && yPosition === idx * 10) {
+      xPosition += 10
+    }
+    if (xPosition === 150 && yPosition < 75) {
+      yPosition += 5
+    }
+    if (yPosition === 75 && xPosition <= 150) {
+      xPosition -= 10
+    }
+    if (xPosition === idx * 10 && yPosition <= 75) {
+      yPosition -= 5
+    }
+    flyer.setCurrentPosition(xPosition, yPosition)
+    ctx.fillStyle = flyer.fillStyle
+    ctx.fillRect(xPosition, yPosition, flyer.dimensions.width, flyer.dimensions.height)
+  })
 }
