a3f6bc6e37d68383e4e7d75808742ffd18695671
adam-kuhn <adam.kuhn20@gmail.com>
2020-04-10 21:08:55 +0300

    set up a simple server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: path.join(__dirname, './client/index.js'),
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, './public')
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, './node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'),
+        to: path.join(__dirname, './public/style/webfonts')
+      },
+      {
+        from: path.join(__dirname, './node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff'),
+        to: path.join(__dirname, './public/style/webfonts')
+      }
+    ])
+  ]
 }
