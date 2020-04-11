a68e6ed5e530aee8a743c449e49358bf19916aaa
adam-kuhn <adam.kuhn20@gmail.com>
2020-04-11 13:12:13 +0300

    remove fontawesome as a dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,17 +6,5 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './public')
-  },
-  plugins: [
-    new CopyPlugin([
-      {
-        from: path.join(__dirname, './node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'),
-        to: path.join(__dirname, './public/style/webfonts')
-      },
-      {
-        from: path.join(__dirname, './node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff'),
-        to: path.join(__dirname, './public/style/webfonts')
-      }
-    ])
-  ]
+  }
 }
