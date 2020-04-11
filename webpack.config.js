34f17da567315b589e5559f4d422b9bff08b2a1d
adam-kuhn <adam.kuhn20@gmail.com>
2020-04-11 13:41:19 +0300

    remove ./ from client/index.js path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, './client/index.js'),
+  entry: path.join(__dirname, 'client/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public')
