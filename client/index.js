2ca8a80d108f1f782ce466045f1284b5df08b161
adam-kuhn <adam.kuhn20@gmail.com>
2020-04-18 09:38:16 +0300

    move math function to utils folder

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import {CANVAS_MARGIN, NEIGHBOUR_RADIUS, DEGREES_TO_RADIANS} from './config/config'
+import {calculateDistanceBetweenTwoFlyers, matchNeighbourhoodVector,
+  getCohesionVector, getSeperationVector} from './utils/vectorHelpers'
 import FlyingAgent from './flyingAgents'
+
 const canvas = document.getElementById('flocking-behaviour')
 const ctx = canvas.getContext('2d')
 ctx.font = '600 10px "Font Awesome 5 Free"'
@@ -40,15 +43,12 @@ function drawCanvas (flyingAgents) {
       return false
     })
     if (flyersInNeighbourhood.length) {
+      // flyer tries to match velocity of other agents in neighbourhood
       const neighbourhoodVector = matchNeighbourhoodVector(flyer, flyersInNeighbourhood)
-      console.log(neighbourhoodVector)
       // cohesion is the the center of all items in neighbourod, and the current agent should rotate to that point
-      // should point of cohesion include the current flyer? NO
       const coehsionVector = getCohesionVector(flyer, flyersInNeighbourhood)
       // sepration is to prevent flyers from overcrowding to one area
-      // this will be an average of the distance bewteen the current flyers and neighbours, and set to a negative value?
       const separationVector = getSeperationVector(flyer, flyersInNeighbourhood)
-      console.log('sep', separationVector)
       const xVeloctiy = neighbourhoodVector.xVelocity + coehsionVector.xPosition + separationVector.xPosition
       const yVelocity = neighbourhoodVector.yVelocity + coehsionVector.yPosition + separationVector.yPosition
       flyer.setVelocity(xVeloctiy, yVelocity)
@@ -80,69 +80,6 @@ function rotateContextBasedOnOrientation (flyer) {
   ctx.translate(xOriginForTranslate, yOriginForTranslate)
   ctx.rotate(flyerOrientation * DEGREES_TO_RADIANS)
   ctx.translate(-xOriginForTranslate, -yOriginForTranslate)
-}
-
-function calculateDistanceBetweenTwoFlyers (currentFlyerPosition, neighbourFlyerPosition) {
-  const yDifference = neighbourFlyerPosition.yPosition - currentFlyerPosition.yPosition
-  const xDifference = neighbourFlyerPosition.xPosition - currentFlyerPosition.xPosition
-  // a^2 + b^2 = c^2
-  const distanceBetweenFlyers = Math.sqrt(Math.pow(yDifference, 2) + Math.pow(xDifference, 2))
-  return distanceBetweenFlyers
-}
-
-function matchNeighbourhoodVector (currentFlyer, neighborhoodFlyers) {
-  const sumOfVelocities = neighborhoodFlyers.reduce((accumulated, current) => {
-    return {
-      xVelocity: accumulated.xVelocity + current.velocity.xVelocity,
-      yVelocity: accumulated.yVelocity + current.velocity.yVelocity
-    }
-  }, {xVelocity: 0, yVelocity: 0})
-  const averagerVelocity = {
-    xVelocity: sumOfVelocities.xVelocity / neighborhoodFlyers.length,
-    yVelocity: sumOfVelocities.yVelocity / neighborhoodFlyers.length
-  }
-  const matchedVelocity = {
-    xVelocity: averagerVelocity.xVelocity - currentFlyer.velocity.xVelocity,
-    yVelocity: averagerVelocity.yVelocity - currentFlyer.velocity.yVelocity
-  }
-  return matchedVelocity
-}
-
-function getCohesionVector (currentFlyer, neighborhoodFlyers) {
-  const aggregateOfPosition = neighborhoodFlyers.reduce((accumulated, current) => {
-    const currentPosition = current.getPosition()
-    return {
-      xPosition: accumulated.xPosition + currentPosition.xPosition,
-      yPosition: accumulated.yPosition + currentPosition.yPosition
-    }
-  }, {xPosition: 0, yPosition: 0})
-  const cohesionPoint = {
-    xPosition: aggregateOfPosition.xPosition / neighborhoodFlyers.length,
-    yPosition: aggregateOfPosition.yPosition / neighborhoodFlyers.length
-  }
-  const currentFlyerPosition = currentFlyer.getPosition()
-
-  const cohesionVector = {
-    xPosition: (cohesionPoint.xPosition - currentFlyerPosition.xPosition) / 100,
-    yPosition: (cohesionPoint.yPosition - currentFlyerPosition.yPosition) / 100
-  }
-  return cohesionVector
-}
-
-function getSeperationVector (currentFlyer, neighborhoodFlyers) {
-  const currentFlyerPosition = currentFlyer.getPosition()
-  const seperationVector = neighborhoodFlyers.reduce((accumulated, current) => {
-    const neighbourPosition = current.getPosition()
-    const distanceBetweenFlyers = calculateDistanceBetweenTwoFlyers(currentFlyerPosition, neighbourPosition)
-    if (distanceBetweenFlyers <= 17) {
-      return {
-        xPosition: accumulated.xPosition - neighbourPosition.xPosition + currentFlyerPosition.xPosition,
-        yPosition: accumulated.yPosition - neighbourPosition.yPosition + currentFlyerPosition.yPosition
-      }
-    }
-    return accumulated
-  }, {xPosition: 0, yPosition: 0})
-  return seperationVector
 }
 
 function checkIfFlyerIsAtCanvasBoundary (flyer) {
---
+++ client/utils/vectorHelpers.js
@@ -0,0 +1,62 @@
+export function calculateDistanceBetweenTwoFlyers (currentFlyerPosition, neighbourFlyerPosition) {
+  const yDifference = neighbourFlyerPosition.yPosition - currentFlyerPosition.yPosition
+  const xDifference = neighbourFlyerPosition.xPosition - currentFlyerPosition.xPosition
+  // a^2 + b^2 = c^2
+  const distanceBetweenFlyers = Math.sqrt(Math.pow(yDifference, 2) + Math.pow(xDifference, 2))
+  return distanceBetweenFlyers
+}
+
+export function matchNeighbourhoodVector (currentFlyer, neighborhoodFlyers) {
+  const sumOfVelocities = neighborhoodFlyers.reduce((accumulated, current) => {
+    return {
+      xVelocity: accumulated.xVelocity + current.velocity.xVelocity,
+      yVelocity: accumulated.yVelocity + current.velocity.yVelocity
+    }
+  }, {xVelocity: 0, yVelocity: 0})
+  const averagerVelocity = {
+    xVelocity: sumOfVelocities.xVelocity / neighborhoodFlyers.length,
+    yVelocity: sumOfVelocities.yVelocity / neighborhoodFlyers.length
+  }
+  const matchedVelocity = {
+    xVelocity: averagerVelocity.xVelocity - currentFlyer.velocity.xVelocity,
+    yVelocity: averagerVelocity.yVelocity - currentFlyer.velocity.yVelocity
+  }
+  return matchedVelocity
+}
+
+export function getCohesionVector (currentFlyer, neighborhoodFlyers) {
+  const aggregateOfPosition = neighborhoodFlyers.reduce((accumulated, current) => {
+    const currentPosition = current.getPosition()
+    return {
+      xPosition: accumulated.xPosition + currentPosition.xPosition,
+      yPosition: accumulated.yPosition + currentPosition.yPosition
+    }
+  }, {xPosition: 0, yPosition: 0})
+  const cohesionPoint = {
+    xPosition: aggregateOfPosition.xPosition / neighborhoodFlyers.length,
+    yPosition: aggregateOfPosition.yPosition / neighborhoodFlyers.length
+  }
+  const currentFlyerPosition = currentFlyer.getPosition()
+
+  const cohesionVector = {
+    xPosition: (cohesionPoint.xPosition - currentFlyerPosition.xPosition) / 100,
+    yPosition: (cohesionPoint.yPosition - currentFlyerPosition.yPosition) / 100
+  }
+  return cohesionVector
+}
+
+export function getSeperationVector (currentFlyer, neighborhoodFlyers) {
+  const currentFlyerPosition = currentFlyer.getPosition()
+  const seperationVector = neighborhoodFlyers.reduce((accumulated, current) => {
+    const neighbourPosition = current.getPosition()
+    const distanceBetweenFlyers = calculateDistanceBetweenTwoFlyers(currentFlyerPosition, neighbourPosition)
+    if (distanceBetweenFlyers <= 17) {
+      return {
+        xPosition: accumulated.xPosition - neighbourPosition.xPosition + currentFlyerPosition.xPosition,
+        yPosition: accumulated.yPosition - neighbourPosition.yPosition + currentFlyerPosition.yPosition
+      }
+    }
+    return accumulated
+  }, {xPosition: 0, yPosition: 0})
+  return seperationVector
+}
